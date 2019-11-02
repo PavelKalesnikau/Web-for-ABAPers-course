08581dee1584d5f2e58e2dfaa06ff7464e695f7c
Pavel.Kalesnikau <pkalesnikau@gmail.com>
2019-11-02 12:21:17 +0300

    version1

---
+++ NODE.JS - part 3/main.js
@@ -0,0 +1,7 @@
+/* Hello, World! program in node.js */
+console.log("Hello, World!");
+console.log(__dirname);
+console.log(__filename);
+setTimeout(function(){
+    console.log("Привет");
+}, 2000);
